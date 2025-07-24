690d3b24a7afc3de3791ed5abf2eff83dab20c0a
Jakub Hlaváček <git@jakubhlavacek.com>
2025-07-24 22:20:17 +0200

    Implement language setting

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,10 +1,17 @@
-import { init, register } from 'svelte-i18n';
+import { init, locale, register } from 'svelte-i18n';
+import { settingsStore } from '../../stores';
+import { get } from 'svelte/store';
 
 export async function initI18n() {
     register('en', () => import('./locales/en.json'));
 
     await init({
-        initialLocale: "en",
-        fallbackLocale: "en"
+        initialLocale: get(settingsStore).language,
+        fallbackLocale: 'en'
+    });
+
+    // Update the locale
+    settingsStore.subscribe(function(value) {
+        locale.set(value.language);
     });
 }
